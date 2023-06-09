e70bf353bbad40b0df46cecfe06f396bb5fe7f0d
brandonliu12 <brandonl.liu@mail.utoronto.ca>
2023-06-08 20:09:04 -0400

    commit-message

--- Web Design/GroceryPricesFlaskApp.py
+++ Web Design/GroceryPricesFlaskApp.py
@@ -1,30 +1,27 @@
-#IMPORTS
-
 from flask import Flask, jsonify, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-#Base Route
-
+# Base Route
 @app.route("/")
 def index(): 
     return render_template("index.html")
 
-#API Route
+# API Route
 @app.route("/api")
 def api():
+    try:
+        # Load the CSV file into a pandas DataFrame
+        df = pd.read_csv('Sorted_Merged_Loblaws_price_comparison.csv')
 
+        # Convert the DataFrame to a dictionary and jsonify it
+        data = df.to_dict(orient='records')
+        return jsonify(data)
+    except Exception as e:
+        print(e)  # print the error to the console for debugging
+        return jsonify({"error": str(e)}), 500  # Return a JSON response with error message and a 500 status code
 
-    #Return Data
-
-    return "data"
-
-    #Return Nothing if Error
-
-    return 0
-
-
-#Run
-
+# Run
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # enable debug mode for more detailed error messages
