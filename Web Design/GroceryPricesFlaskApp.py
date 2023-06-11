7faf43e825ff1617a741ba4b12ddf4ffd261f924
Pradanthony <125171223+Pradanthony@users.noreply.github.com>
2023-06-10 23:00:41 -0400

    api.py

--- Web Design/GroceryPricesFlaskApp.py
+++ Web Design/GroceryPricesFlaskApp.py
@@ -1,33 +1,25 @@
-from flask import Flask, jsonify, render_template
 import pandas as pd
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-# Load the data at the start of the application
-df = pd.read_csv('Sorted_Merged_Loblaws_price_comparison.csv')
+# Load the sorted and merged DataFrame
+df_sorted_merged = pd.read_csv(r'C:\Users\Pradivan\Documents\GitHub\grocery-prices-comparison\Web Design\Sorted_Merged_Loblaws_price_comparison.csv')
 
-@app.route('/')
-def main():
-    # This is the main page
-    return render_template('main.html')
+# Base Route
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/view_data')
-def view_data():
-    # Render the DataFrame in an HTML template
-    return render_template('data.html', data=df.to_html())
+# API Route
+@app.route("/api")
+def api():
+    # Convert DataFrame to JSON
+    data = df_sorted_merged.to_dict(orient='records')
 
-@app.route('/charts')
-def charts():
-    # Page for dynamic charts
-    # This will depend on what charting library you want to use
-    # And what data you want to display
-    return render_template('charts.html', data=df.to_dict(orient='records'))
+    # Return JSON data
+    return jsonify(data)
 
-@app.route('/maps')
-def maps():
-    # Page for leaflet plots
-    # The implementation will depend on how you want to plot your data
-    return render_template('maps.html', data=df.to_dict(orient='records'))
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Run
+if __name__ == "__main__":
+    app.run()
