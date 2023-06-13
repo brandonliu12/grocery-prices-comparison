c29fdec1e87c64cb92c95890d05fdcd31f480b53
ChrisC <chenchristopher27@gmail.com>
2023-06-12 20:56:16 -0400

    updated web files

--- Web Design/GroceryPricesFlaskApp.py
+++ Web Design/GroceryPricesFlaskApp.py
@@ -1,33 +1,32 @@
-from flask import Flask, jsonify, render_template
 import pandas as pd
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import math
+import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
-# Load the data at the start of the application
-df = pd.read_csv('Web Scrapping\Sorted_Merged_Loblaws_price_comparison.csv')
+# Load the sorted and merged DataFrame
+df_sorted_merged = pd.read_csv('Final_Merged_Loblaws_price_comparison.csv')
 
-@app.route('/')
-def main():
-    # This is the main page
-    return render_template('main.html')
+# Replace NaN values with a specific string
+df_sorted_merged.replace(np.nan, 'N/A', inplace=True)
 
-@app.route('/view_data')
-def view_data():
-    # Render the DataFrame in an HTML template
-    return render_template('data.html', data=df.to_html())
+# Base Route
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/charts')
-def charts():
-    # Page for dynamic charts
-    # This will depend on what charting library you want to use
-    # And what data you want to display
-    return render_template('charts.html', data=df.to_dict(orient='records'))
+# API Route
+@app.route("/api")
+def api():
+    # Convert DataFrame to a list of dictionaries
+    data = df_sorted_merged.to_dict(orient='records')
 
-@app.route('/maps')
-def maps():
-    # Page for leaflet plots
-    # The implementation will depend on how you want to plot your data
-    return render_template('maps.html', data=df.to_dict(orient='records'))
+    # Return JSON data
+    return jsonify(data)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Run
+if __name__ == "__main__":
+    app.run()
